f145c5b84f790e5ddf3cdc1fe22a5b3ed1a09a0a
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-09-06 10:53:22 +0300

    LB1 upload
    Did something

---
+++ LB1/IISHandler1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Web;
+
+namespace LB1
+{
+    public class IISHandler1 : IHttpHandler
+    {
+        /// <summary>
+        /// Вам потребуется настроить этот обработчик в файле Web.config вашего 
+        /// веб-сайта и зарегистрировать его с помощью IIS, чтобы затем воспользоваться им.
+        /// см. на этой странице: https://go.microsoft.com/?linkid=8101007
+        /// </summary>
+        #region Члены IHttpHandler
+
+        public bool IsReusable
+        {
+            // Верните значение false в том случае, если ваш управляемый обработчик не может быть повторно использован для другого запроса.
+            // Обычно значение false соответствует случаю, когда некоторые данные о состоянии сохранены по запросу.
+            get { return true; }
+        }
+
+        public int RESULT = 0;
+
+        public void ProcessRequest(HttpContext context)
+        {
+            HttpRequest req = context.Request;
+            HttpResponse res = context.Response;
+
+
+            if (req.HttpMethod.Equals("GET"))
+            {
+                res.AddHeader("Content-Type", "application/json");
+                res.Write($"{RESULT}");
+            }
+
+            if (req.HttpMethod.Equals("POST") && req.Params["RESULT"] != null)
+            {
+                RESULT = Convert.ToInt32(req.Params["RESULT"]);
+                res.Write($"{RESULT}");
+            }
+        }
+
+        #endregion
+    }
+}
