c8fe9843decf8e4a3f7a977cc0bfaed69ea2f18b
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-09-10 10:05:47 +0300

    Should test RESULT in session context

--- LB1/IISHandler1.cs
+++ LB1/IISHandler1.cs
@@ -20,65 +20,46 @@ namespace LB1
             get { return true; }
         }
 
-        public int RESULT = 0;
-        public Stack<int> numbersStack = new Stack<int>();
-
         public void ProcessRequest(HttpContext context)
         {
+            int RESULT = 0;
             HttpRequest req = context.Request;
             HttpResponse res = context.Response;
+            res.AddHeader("Content-Type", "application/json");
 
-            if(context.Session["sessionStack"] == null)
+            if (context.Session["sessionStack"] == null)
+            {
+                Stack<int> numbersStack = new Stack<int>();
                 context.Session.Add("sessionStack", numbersStack);
-
-            //res.AddHeader("Content-Type", "application/json");
-
+            }
+                
             if (req.HttpMethod.Equals("GET"))
             {
-                Stack<int> tempNumbersStack = (Stack<int>)context.Session["sessionStack"];
                 try
                 {
-                    RESULT += tempNumbersStack.Peek();
+                    RESULT += (context.Session["sessionStack"] as Stack<int>).Peek();
                 }
                 catch (InvalidOperationException e)
                 {
                     RESULT += 0;
                 }
                 res.Write($"{RESULT}");
-                res.Write("" +
-                    "<form action='http://localhost:61592/FAA.XXX/' method='put'>" +
-                    "<label for='PUT-name'>ADD value:</label>" +
-                    "<br>" +
-                    "<input id='PUT-name type='number' name='ADD'>" +
-                    "<br><br>" +
-                    "<input type='submit' value='Add to stack'>" +
-                    "</form>");
             }
 
             if (req.HttpMethod.Equals("POST") && req.Params["RESULT"] != null)
             {
                 RESULT = Convert.ToInt32(req.Params["RESULT"]);
-                res.Write($"{RESULT}");
             }
 
             if (req.HttpMethod.Equals("PUT") && req.Params["ADD"] != null)
             {
-                //context.Session["sessionStack"] = numbersStack;
                 int toPush = Convert.ToInt32(req.Params["ADD"]);
-                //context.Session.Add("sessionStack", numbersStack);
-                Stack<int> tempNumbersStack = (Stack<int>)context.Session["sessionStack"];
-                //numbersStack.Push(toPush);
-                //res.Write($"{numbersStack.Peek()}");
-                tempNumbersStack.Push(toPush);
-                context.Session["sessionStack"] = tempNumbersStack;
-                //res.Write($"<h2>{tempNumbersStack.Peek()}</h2>");
+                (context.Session["sessionStack"] as Stack<int>).Push(toPush);
             }
 
             if (req.HttpMethod.Equals("DELETE"))
             {
-                Stack<int> tempNumbersStack = (Stack<int>)context.Session["sessionStack"];
-                tempNumbersStack.Pop();
-                res.Write($"{RESULT}");
+                (context.Session["sessionStack"] as Stack<int>).Pop();
             }
         }
 
