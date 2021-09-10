7b6af68b4a5f75538443458e0a7aa220ab2ac0a7
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-09-10 08:29:21 +0300

    Going to work with session

--- LB1/IISHandler1.cs
+++ LB1/IISHandler1.cs
@@ -1,9 +1,10 @@
 ﻿using System;
 using System.Web;
+using System.Collections.Generic;
 
 namespace LB1
 {
-    public class IISHandler1 : IHttpHandler
+    public class IISHandler1 : IHttpHandler, System.Web.SessionState.IRequiresSessionState
     {
         /// <summary>
         /// Вам потребуется настроить этот обработчик в файле Web.config вашего 
@@ -20,22 +21,35 @@ namespace LB1
         }
 
         public int RESULT = 0;
+        public Stack<int> numbersStack = new Stack<int>();
 
         public void ProcessRequest(HttpContext context)
         {
             HttpRequest req = context.Request;
             HttpResponse res = context.Response;
 
+            res.AddHeader("Content-Type", "application/json");
 
             if (req.HttpMethod.Equals("GET"))
-            {
-                res.AddHeader("Content-Type", "application/json");
+            { 
                 res.Write($"{RESULT}");
             }
 
             if (req.HttpMethod.Equals("POST") && req.Params["RESULT"] != null)
             {
                 RESULT = Convert.ToInt32(req.Params["RESULT"]);
+                res.Write($"{RESULT}");
+            }
+
+            if (req.HttpMethod.Equals("PUT") && req.Params["ADD"] != null)
+            {
+                int toPush = Convert.ToInt32(req.Params["ADD"]);
+                numbersStack.Push(toPush);
+                res.Write($"{numbersStack.Peek()}");
+            }
+
+            if (req.HttpMethod.Equals("DELETE"))
+            {
                 res.Write($"{RESULT}");
             }
         }
