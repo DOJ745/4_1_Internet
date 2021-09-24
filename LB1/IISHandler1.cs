3e2e9f3f3b61518e9fc2dd9b74434cc29915a51c
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-09-24 08:40:33 +0300

    Removed comments and prev page

--- LB1/IISHandler1.cs
+++ LB1/IISHandler1.cs
@@ -26,9 +26,6 @@ namespace LB1
         {
             if (context.Session["sessionStack"] == null)
             {
-                //Stack<int> numbersStack = new Stack<int>();
-                //context.Session.Add("sessionStack", numbersStack);
-
                 context.Session["sessionStack"] = new Stack<int>();
             }
 
@@ -50,62 +47,6 @@ namespace LB1
                     context.Response.Write("Error");
                     break;
             }
-
-            /*
-            if (req.HttpMethod.Equals("GET"))
-            {
-                try
-                {
-                    res.Write($"{RESULT + (context.Session["sessionStack"] as Stack<int>).Peek()}");
-                }
-                catch (InvalidOperationException e)
-                {
-                    res.Write($"{RESULT}");
-                }
-            }
-
-            if (req.HttpMethod.Equals("POST") && req.Params["RESULT"] != null)
-            {
-                RESULT = Convert.ToInt32(req.Params["RESULT"]);
-                res.Write($"{RESULT}");
-            }
-
-            if (
-                ( req.Form.Get("_method") != null &&
-                req.Form.Get("_method").Equals("PUT") &&
-                req.Params["ADD"] != null ) ||
-                req.HttpMethod.Equals("PUT"))
-            {
-
-                int toPush = Convert.ToInt32(req.Params["ADD"]);
-                try
-                {
-                    (context.Session["sessionStack"] as Stack<int>).Push(toPush);
-                    res.Write($"{(context.Session["sessionStack"] as Stack<int>).Peek()} " +
-                        $"--- {context.Session.IsNewSession}");
-                }
-                catch (InvalidOperationException e)
-                {
-                    res.Write($"No stack!");
-                }
-            }
-
-            if (
-                (req.Form.Get("_method") != null &&
-                req.Form.Get("_method").Equals("DELETE") ) || 
-                req.HttpMethod.Equals("DELETE") 
-                )
-            {
-                try
-                {
-                    int popped = (context.Session["sessionStack"] as Stack<int>).Pop();
-                    //res.Write($"{RESULT + (context.Session["sessionStack"] as Stack<int>).Peek()}");
-                }
-                catch (InvalidOperationException e)
-                {
-                    res.Write($"No stack! - {RESULT}");
-                }
-            }*/
         }
 
         private void Get(HttpContext context)
@@ -137,15 +78,8 @@ namespace LB1
         {
             try
             {
-                /*if (
-                (context.Request.Form.Get("_method") != null &&
-                context.Request.Form.Get("_method").Equals("PUT") &&
-                context.Request.Params["ADD"] != null) ||
-                context.Request.HttpMethod.Equals("PUT"))
-                {*/
-                    int newValue = int.Parse(context.Request.Params["ADD"]);
-                    (context.Session["sessionStack"] as Stack<int>).Push(newValue);
-                //}                
+                int newValue = int.Parse(context.Request.Params["ADD"]);
+                (context.Session["sessionStack"] as Stack<int>).Push(newValue);      
             }
             catch (Exception) { }
         }
@@ -154,14 +88,7 @@ namespace LB1
         {
             try
             {
-                /*if (
-                (context.Request.Form.Get("_method") != null &&
-                context.Request.Form.Get("_method").Equals("DELETE")) ||
-                context.Request.HttpMethod.Equals("DELETE")
-                )
-                {*/
-                    int popped = (context.Session["sessionStack"] as Stack<int>).Pop();
-                //}
+                int popped = (context.Session["sessionStack"] as Stack<int>).Pop();
             }
             catch (Exception) { }
         }
