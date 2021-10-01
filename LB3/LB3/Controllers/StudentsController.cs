8260387d999561b4d7f84224149190852e4b6777
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-10-01 10:22:15 +0300

    Did some for LB3
    Database, model, context, error handler

---
+++ LB3/LB3/Controllers/StudentsController.cs
@@ -0,0 +1,46 @@
+﻿using LB3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace LB3.Controllers
+{
+    public class StudentsController : ApiController
+    {
+        private string tempValue = "RESPONSE";
+        private StudentsContext DB = new StudentsContext();
+
+        // GET api/students
+        public IHttpActionResult Get()
+        {
+            return Json(new { tempValue });
+        }
+
+        // GET api/students/5
+        public IHttpActionResult Get(int id)
+        {
+            return Json(new { tempValue });
+        }
+
+        // POST api/students
+        public IHttpActionResult Post([FromBody] string value)
+        {
+            return Json(new { tempValue });
+        }
+
+        // PUT api/students/5
+        public IHttpActionResult Put(int id, [FromBody] string value)
+        {
+            return Json(new { tempValue });
+        }
+
+        // DELETE api/students/5
+        public IHttpActionResult Delete(int id)
+        {
+            return Json(new { tempValue });
+        }
+    }
+}
---
+++ LB3/LB3/Models/StudentsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace LB3.Models
+{
+    public class StudentsContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            Database.SetInitializer<StudentsContext>(null);
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
