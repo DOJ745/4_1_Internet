b701f110258f0be4948a3b8483b52701845bcc59
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-10-18 09:00:13 +0300

    Did main GET request(for search)

--- LB3/LB3/Controllers/StudentsController.cs
+++ LB3/LB3/Controllers/StudentsController.cs
@@ -1,5 +1,6 @@
 ﻿using LB3.Models;
 using System.Web.Http;
+using System.Web.WebPages;
 using System.Runtime.Serialization;
 using System.Xml.Serialization;
 using System.IO;
@@ -7,6 +8,7 @@ using Newtonsoft.Json;
 using System.Linq;
 using System;
 using System.Net;
+using System.Collections.Generic;
 
 namespace LB3.Controllers
 {
@@ -24,6 +26,85 @@ namespace LB3.Controllers
         }
 
         // GET api/students
+        public IHttpActionResult GetStudents(
+            string name = null,
+            string columns = "",
+            string phone = null,
+
+            int offset = 0,
+            int limit = 45,
+
+            string globalLike = "",
+            string orderby = "off",
+
+            int minId = Int32.MinValue,
+            int maxId = Int32.MaxValue
+        )
+        {
+            var students = new List<Student>();
+            var sqlQuery = string.Empty;
+            string orderBy;
+
+            if (orderby.Equals("on")) { orderBy = "Name"; }
+            else { orderBy = "Id"; }
+
+            if (globalLike.IsEmpty())
+            {
+                string sqlName = "";
+                if (name != null) { sqlName = " AND Name LIKE '%' + @P2 + '%' "; }
+
+                string sqlPhone = "";
+                if (phone != null) { sqlPhone = "AND Phone = @P3 "; }
+
+                sqlQuery = "SELECT * FROM Student " +
+                    "WHERE ID > @P0 AND ID < @P1 " + sqlName + sqlPhone +
+                    "ORDER BY " + orderBy + " " +
+                    "OFFSET @P4 ROWS FETCH NEXT @P5 ROWS ONLY";
+
+                students = DB.Students.SqlQuery(sqlQuery, 
+                    minId, 
+                    maxId, 
+                    name, 
+                    phone, 
+                    offset, 
+                    limit).ToList();
+            }
+            else
+            {
+                sqlQuery = "SELECT * FROM Student " +
+                    "WHERE ID > @P0 AND " +
+                    "ID < @P1 " +
+                    "AND (ID LIKE '%' + @P2 + '%' OR NAME LIKE '%' + @P2 + '%' " +
+                    "OR PHONE LIKE '%' + @P2 + '%') " +
+                    "ORDER BY " + orderBy + " " +
+                    "OFFSET @P3 ROWS FETCH NEXT @P4 ROWS ONLY";
+
+                students = DB.Students.SqlQuery(sqlQuery, 
+                    minId, 
+                    maxId, 
+                    globalLike, 
+                    offset, 
+                    limit).ToList();
+            }
+
+            string linkStudents = Request.RequestUri.GetLeftPart(UriPartial.Path);
+
+            students.ForEach(student => student._links = 
+                new HateoasLinks(linkStudents, linkStudents + "/" + student.ID));
+
+            columns = columns.ToLower();
+
+            if (!columns.Contains("name"))
+                students.ForEach(x => x.NAME = null);
+
+            if (!columns.Contains("phone"))
+                students.ForEach(x => x.PHONE = null);
+
+            if (!columns.Contains("id"))
+                students.ForEach(x => x.ID = 0);
+
+            return Ok(students);
+        }
 
         // GET api/students/5
         [Route("api/Students/{id}")]
