0752e52b3ec518f31ba64317fe0ad648f0b66ce6
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-10-27 09:38:56 +0300

    Did simple ASMX service for LB4

---
+++ LB4/LB4/SimpleClass.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LB4
+{
+    public class SimpleClass
+    {
+        public string str { get; set; }
+        public int numberInt { get; set; }
+        public float numberFloat { get; set; }
+    }
+}
---
+++ LB4/LB4/Simplex.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace LB4
+{
+    [WebServiceBinding(ConformsTo = WsiProfiles.None)]
+    [WebService(Description = "Simplex web service")]
+    public class Simplex : WebService
+    {
+        [WebMethod(Description = "Sum of two integer")]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        [WebMethod(Description = "Concatenation of string and double")]
+        public string Concat(string str, double numberDouble)
+        {
+            return str + numberDouble;
+        }
+
+        [WebMethod(Description = "Sum of two classes")]
+        public SimpleClass Sum(SimpleClass objOne, SimpleClass objTwo)
+        {
+            SimpleClass result = new SimpleClass();
+
+            result.str = objOne.str + objTwo.str;
+            result.numberInt = objOne.numberInt + objTwo.numberInt;
+            result.numberFloat = objOne.numberFloat + objTwo.numberFloat;
+
+            return result;
+        }
+
+        [WebMethod]
+        public TestReport GetReport(int reportID)
+        {
+            TestReport testReport = new TestReport()
+            {
+                ReportID = reportID,
+                Date = DateTime.Now,
+                Info = "Some info"
+            };
+
+            return testReport;
+        }
+    }
+
+    public class TestReport
+    {
+        public int ReportID { get; set; }
+        public DateTime Date { get; set; }
+        public string Info { get; set; }
+    }
+}
