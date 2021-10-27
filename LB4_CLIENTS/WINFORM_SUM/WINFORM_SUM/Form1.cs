3af5e3c6582feccfdea9e6d5be641d580a8a4407
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-10-27 12:34:10 +0300

    Did some for client

--- LB4_CLIENTS/WINFORM_SUM/WINFORM_SUM/Form1.cs
+++ LB4_CLIENTS/WINFORM_SUM/WINFORM_SUM/Form1.cs
@@ -1,52 +1,71 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace WINFORM_SUM
 {
-    public class SOAPRequest
-    {
-        public string str { get; set; }
-        public int numberInt { get; set; }
-        public float numberFloat { get; set; }
-    }
-
     public partial class Form1 : Form
     {
+        SOAPRequest objOne = new SOAPRequest();
+        SOAPRequest objTwo = new SOAPRequest();
         public Form1()
         {
             InitializeComponent();
         }
 
-        private void label2_Click(object sender, EventArgs e)
-        {
-
-        }
-
         private void button1_Click(object sender, EventArgs e)
         {
+            using (var webClient = new WebClient())
+            {
+                var reqParams = new NameValueCollection();
+                string address = "http://localhost:/Simplex.asmx";
 
+                //reqParams.Add("x", firstParam);
+                //reqParams.Add("y", secondParam);
+
+                var response = webClient.UploadValues(address, reqParams);
+                string str = Encoding.UTF8.GetString(response);
+                MessageBox.Show(str);
+            }
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-
+            objOne.numberInt = int.Parse(textBox1.Text);
         }
 
         private void textBox2_TextChanged(object sender, EventArgs e)
         {
-
+            objOne.numberFloat = float.Parse(textBox2.Text);
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-
+            objOne.str = textBox3.Text;
         }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            objTwo.numberInt = int.Parse(textBox4.Text);
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            objTwo.numberFloat = float.Parse(textBox5.Text);
+        }
+
+        private void textBox6_TextChanged(object sender, EventArgs e)
+        {
+            objTwo.str = textBox6.Text;
+        }
+
     }
 }
---
+++ LB4_CLIENTS/WINFORM_SUM/WINFORM_SUM/SOAPRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WINFORM_SUM
+{
+    public class SOAPRequest
+    {
+        public string str { get; set; }
+        public int numberInt { get; set; }
+        public float numberFloat { get; set; }
+
+        public string formSOAP(SOAPRequest otherObject)
+        {
+            string SOAP = "<?xml version=\"1.0\" encoding=\"utf-8\"?>\n" +
+"<soap:Envelope xmlns:xsi=\"http://www.w3.org/2001/XMLSchema-instance\" xmlns:xsd=\"http://www.w3.org/2001/XMLSchema\" xmlns:soap=\"http://schemas.xmlsoap.org/soap/envelope/\">\n" +
+"  <soap:Body>\n" +
+"    <Sum xmlns=\"FAA\">\n" +
+"      <objOne>\n" +
+"        <str>" + this.str +"</str>\n" +
+"        <numberInt>" + this.numberInt + "</numberInt>\n" +
+"        <numberFloat>" + this.numberFloat + "</numberFloat>\n" +
+"      </objOne>\n" +
+"      <objTwo>\n" +
+"        <str>" + otherObject.str + "</str>\n" +
+"        <numberInt>" + otherObject.numberInt + "</numberInt>\n" +
+"        <numberFloat>" + otherObject.numberFloat + "</numberFloat>\n" +
+"      </objTwo>\n" +
+"    </Sum>\n" +
+"  </soap:Body>\n" +
+"</soap:Envelope>";
+            return SOAP;
+        }
+    }
+}
