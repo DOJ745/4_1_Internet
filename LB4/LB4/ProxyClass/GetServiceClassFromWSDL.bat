3406166d3e533d8133e15b8184461221e9a0f40b
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-11-01 13:33:28 +0300

    Generated class by wsdl annotation

--- LB4/LB4/ProxyClass/GetServiceClassFromWSDL.bat
+++
@@ -1,2 +0,0 @@
-wsdl /n:WINFORM_WSDL_SERVICE http://localhost:63964/Simplex.asmx?op=Concat
-pause
---
+++ LB4/LB4_BY_WSDL/Simplex.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Web.Services.Protocols;
+
+namespace LB4_BY_WSDL
+{
+    [WebService(Namespace = "FAA")]
+    [WebServiceBinding(Name = "SimplexSoap", Namespace = "FAA")]
+    public class Simplex : WebService
+    {
+        [WebMethod]
+        [SoapDocumentMethod("FAA/Add", RequestNamespace = "FAA", ResponseNamespace = "FAA", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped)]
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+
+        /// <remarks/>
+        [WebMethod]
+        [SoapDocumentMethod("FAA/AddS", RequestNamespace = "FAA", ResponseNamespace = "FAA", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped)]
+        public string AddS(int x, int y)
+        {
+            int sum = x + y;
+            return "{d:" + sum +"}";
+        }
+
+        /// <remarks/>
+        [WebMethod()]
+        [SoapDocumentMethod("FAA/Concat", RequestNamespace = "FAA", ResponseNamespace = "FAA", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped)]
+        public string Concat(string str, double numberDouble)
+        {
+            return str + numberDouble;
+        }
+
+        /// <remarks/>
+        [WebMethod]
+        [SoapDocumentMethod("FAA/Sum", RequestNamespace = "FAA", ResponseNamespace = "FAA", Use = System.Web.Services.Description.SoapBindingUse.Literal, ParameterStyle = System.Web.Services.Protocols.SoapParameterStyle.Wrapped)]
+        public SimpleClass Sum(SimpleClass objOne, SimpleClass objTwo)
+        {
+            SimpleClass result = new SimpleClass();
+
+            result.str = objOne.str + objTwo.str;
+            result.numberInt = objOne.numberInt + objTwo.numberInt;
+            result.numberFloat = objOne.numberFloat + objTwo.numberFloat;
+
+            /*string reqBody;
+
+            Context.Request.InputStream.Position = 0;
+            StreamReader reader = new StreamReader(Context.Request.InputStream);
+            reqBody = reader.ReadToEnd();
+
+            result.str += "\n\n" + reqBody + "\n\n";*/
+
+            return result;
+        }
+    }
+
+    public partial class SimpleClass
+    {
+
+        private string strField;
+
+        private int numberIntField;
+
+        private float numberFloatField;
+
+        /// <remarks/>
+        public string str
+        {
+            get
+            {
+                return this.strField;
+            }
+            set
+            {
+                this.strField = value;
+            }
+        }
+
+        /// <remarks/>
+        public int numberInt
+        {
+            get
+            {
+                return this.numberIntField;
+            }
+            set
+            {
+                this.numberIntField = value;
+            }
+        }
+
+        /// <remarks/>
+        public float numberFloat
+        {
+            get
+            {
+                return this.numberFloatField;
+            }
+            set
+            {
+                this.numberFloatField = value;
+            }
+        }
+    }
+}
