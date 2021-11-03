85057db54161a168fe6cb73244b2d0f76f0035bc
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-11-03 18:47:29 +0300

    Did HttpEndpoint and template for TcpEndpoint

---
+++ LB5/HttpEndpoint/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace HttpEndpoint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WCFSiplexClient client = new WCFSiplexClient();
+
+            Console.WriteLine("Input x: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Input y: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+
+            int result = client.Add(x, y);
+            Console.WriteLine($"{x} + {y} = " + result);
+            Console.ReadLine();
+            // Всегда закройте клиент.
+            client.Close();
+        }
+    }
+}
