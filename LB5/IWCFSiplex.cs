5cd7963c6a6210974a05fcaf84c39689e5945f56
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-11-03 18:31:19 +0300

    Doing things with WCF(LB5)
    Did client class by SvcUtils.exe

---
+++ LB5/IWCFSiplex.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LB5
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени интерфейса "IService1" в коде и файле конфигурации.
+    [ServiceContract]
+    public interface IWCFSiplex
+    {
+
+        [OperationContract]
+        int Add(int x, int y);
+
+        [OperationContract]
+        string Concat(string str, double d);
+
+        [OperationContract]
+        A Sum(A objOne, A objTwo);
+
+    }
+
+    [DataContract]
+    public class A
+    {
+        [DataMember]
+        public string s { get; set; }
+
+        [DataMember]
+        public int k { get; set; }
+
+        [DataMember]
+        public float f { get; set; }
+    }
+}
