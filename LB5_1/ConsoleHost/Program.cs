754c21200fcf6a34f513347c3eae1e2905400e7f
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-11-05 08:08:07 +0300

    LB5 is done
    Console host works

--- LB5_1/ConsoleHost/Program.cs
+++ LB5_1/ConsoleHost/Program.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using LB5_1;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,16 @@ namespace ConsoleHost
     {
         static void Main(string[] args)
         {
+            using (ServiceHost host = new ServiceHost(typeof(WCFSiplex)))
+            {
+                host.Open();
+                if(host.State == CommunicationState.Opened)
+                {
+                    Console.WriteLine("PRESS A BUTTON TO CLOSE HOST\n");
+                    Console.WriteLine("HOST STATUS: " + host.State);
+                }
+                Console.ReadLine();
+            }
         }
     }
 }
