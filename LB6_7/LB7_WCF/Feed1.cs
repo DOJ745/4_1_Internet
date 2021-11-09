3a65cf7c3ab02aa056d45922c58c45a305921684
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-11-09 16:10:22 +0300

    Did WCF Syndication Service

---
+++ LB6_7/LB7_WCF/Feed1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Syndication;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace LB7_WCF
+{
+    // ПРИМЕЧАНИЕ. Команду "Переименовать" в меню "Рефакторинг" можно использовать для одновременного изменения имени класса "Service1" в коде и файле конфигурации.
+    public class Feed1 : IFeed1
+    {
+        private LB6_7.WSSDAEntities entities;
+
+        public Feed1()
+        {
+            entities = new LB6_7.WSSDAEntities();
+        }
+        public SyndicationFeedFormatter CreateFeed()
+        {
+            // Создать новый веб-канал.
+            SyndicationFeed feed = new SyndicationFeed(
+                title: "FEED",
+                description: "WCF Syndication Feed",
+                feedAlternateLink: new Uri("http://localhost:8733/Design_Time_Addresses/WCFSyndicationService/Feed1/"),
+                id: "id",
+                lastUpdatedTime: new DateTimeOffset(DateTime.Now),
+                items: GetNewsItems());
+
+            // Возвращать канал ATOM или RSS, основываясь на строке запроса
+            // RSS-&gt; http://localhost:8733/Design_Time_Addresses/WCFSyndicationService/Feed1/
+            // Atom-&gt; http://localhost:8733/Design_Time_Addresses/WCFSyndicationService/Feed1/?format=atom
+            string query = WebOperationContext.Current.IncomingRequest.UriTemplateMatch.QueryParameters["format"];
+            SyndicationFeedFormatter formatter = null;
+            if (query == "atom")
+            {
+                formatter = new Atom10FeedFormatter(feed);
+            }
+            else
+            {
+                formatter = new Rss20FeedFormatter(feed);
+            }
+
+            return formatter;
+        }
+
+        private List<SyndicationItem> GetNewsItems()
+        {
+            List<SyndicationItem> items = new List<SyndicationItem>();
+            foreach (var note in entities.Note.AsEnumerable())
+            {
+                var student = (from stud in entities.Student where stud.id == note.studentId select stud).First();
+                items.Add(new SyndicationItem(
+                    title: note.subject + " : " + note.note1,
+                    content: student.name.ToString(),
+                    itemAlternateLink: new Uri("http://localhost:50620/WcfDataService.svc/Notes(" + note.id + ")"),
+                    id: student.id.ToString(),
+                    lastUpdatedTime: DateTime.Now));
+            }
+            return items;
+        }
+    }
+}
