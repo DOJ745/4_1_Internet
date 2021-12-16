0bf365ee32e1f572f3e244b5db578d23a5c79f70
DOJ745 <55020503+DOJ745@users.noreply.github.com>
2021-12-16 21:52:14 +0300

    Did simple client for LB7

---
+++ LB7_CLIENT/LB7_CLIENT/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.ServiceModel.Syndication;
+using System.Xml;
+
+namespace LB7_CLIENT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string result = "";
+
+            XmlReader xmlReader = XmlReader.Create("http://localhost:8733/Design_Time_Addresses/LB7_WCF/Feed1");
+            SyndicationFeed feed = SyndicationFeed.Load(xmlReader);
+            foreach (SyndicationItem item in feed.Items)
+            {
+                result += item.Title.Text + "\n";
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
